4635a6e138d4286ab560d55f8c0a484eff095e8a
chinupython <chinukakati188@gmail.com>
2025-08-24 12:52:54 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number : "))
+
+
+def factorial(n):
+    if n<2 :
+        return 1
+    else:
+        return n*factorial(n-1)
+result=factorial(num)
+print("Factorial of " ,num ," is: ",result)
---
+++ Task2.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+
+from math import *
+print("square root " ,sqrt(num))
+print("logarithm " ,log(num))
+print("sinh " ,sin(num))
